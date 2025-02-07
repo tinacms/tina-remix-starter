a901b87a8ab231d69b8011a1f22b2959ae66c828
Isaac Lombard <isaaclombard@ssw.com.au>
2025-02-07 16:13:05 +1100

    banner with RGB effects

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -83,6 +83,23 @@ const Icon = {
       />
     </svg>
   ),
+  deployment: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
+    >
+      <path
+        d="M17.5 12.5V15.8333C17.5 16.2754 17.3244 16.6993 17.0118 17.0118C16.6993 17.3244 16.2754 17.5 15.8333 17.5H4.16667C3.72464 17.5 3.30072 17.3244 2.98816 17.0118C2.67559 16.6993 2.5 16.2754 2.5 15.8333V12.5M10 13.3333L10 2.5M10 2.5L5.83333 6.66667M10 2.5L14.1667 6.66667"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
 };
 
 export default function Index() {
@@ -93,8 +110,8 @@ export default function Index() {
   const page = data.page;
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
+    <div className="flex flex-col h-screen items-center justify-between">
+      <div className="flex flex-col items-center gap-16 my-auto">
         <header className="flex flex-col items-center gap-9">
           <h1
             data-tina-field={tinaField(page, "header")}
@@ -140,6 +157,20 @@ export default function Index() {
             ))}
           </ul>
         </nav>
+      </div>
+      <div className="flex w-full justify-center py-6 relative overflow-hidden dark:bg-gray-950 bg-white">
+        <div className="absolute inset-0 bg-footer-gradient-light dark:bg-footer-gradient-dark animate-wave" />
+        <div className="flex items-center gap-2 text-xl relative z-10">
+          <img src="/llama.svg" alt="Tina CMS Llama Logo" className="w-[30px] h-auto" />
+          <a 
+            href="https://tina.io" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="text-gray-700 dark:text-gray-200 no-underline pt-2 hover:text-[#EC4815]"
+          >
+            Powered by TinaCMS
+          </a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
@@ -16,6 +17,44 @@ export default {
           '"Noto Color Emoji"',
         ],
       },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'translateX(-100%)' },
+          '45%': { transform: 'translateX(100%)' },
+          '55%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        }
+      },
+      animation: {
+        'wave': 'wave 6s cubic-bezier(0.45, 0.05, 0.55, 0.95) infinite'
+      },
+      backgroundImage: {
+        'footer-gradient-light': `linear-gradient(
+          to bottom, 
+          rgba(255, 255, 255, 0),
+          rgba(255, 255, 255, 1)
+        ),
+        linear-gradient(
+          50deg,
+          transparent 0%,
+          rgba(255, 255, 255, 0) 10%,
+          rgb(57, 146, 255, 0.2) 30%,
+          rgb(107, 217, 104, 0.2) 40%,
+          rgb(254, 204, 27, 0.3) 50%,
+          rgb(216, 59, 210, 0.2) 60%,
+          rgb(244, 66, 80, 0.2) 70%,
+          rgba(255, 255, 255, 0) 90%,
+          transparent 100%
+        )`,
+        'footer-gradient-dark': `linear-gradient(
+          90deg,
+          transparent 0%,
+          rgba(255, 255, 255, 0) 20%,
+          rgba(255, 255, 255, 0.1) 50%,
+          rgba(255, 255, 255, 0) 80%,
+          transparent 100%
+        )`
+      }
     },
   },
   plugins: [],
